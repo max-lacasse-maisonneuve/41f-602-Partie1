75e900aeef10c667261a97480550a36ff101f481
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2024-04-10 10:35:58 -0400

    Cours 2 fin

--- backend - API/server.js
+++ backend - API/server.js
@@ -1,5 +1,7 @@
 const http = require("http");
 const fs = require("fs");
+const path = require("path");
+
 const express = require("express");
 const server = express();
 
@@ -7,27 +9,38 @@ const server = express();
 const dotenv = require("dotenv");
 dotenv.config();
 
-server.get("/", (req, res) => {
-    let user = {
-        id: 1,
-        nom: "Maxime",
-        email: "mlacassegermain@cmaisonneuve.qc.ca",
-    };
-    // res.statusCode = 201; //Change le code de status
-    res.status(201).json(user); //Renvoie de la données en json
+//On indique que le dossier public est accessible à tous
+server.use(express.static(path.join(__dirname, "public")));
+
+// server.get("/", (req, res) => {
+//     let user = {
+//         id: 1,
+//         nom: "Maxime",
+//         email: "mlacassegermain@cmaisonneuve.qc.ca",
+//     };
+//     // res.statusCode = 201; //Change le code de status
+//     res.status(201).json(user); //Renvoie de la données en json
+// });
+
+server.get("/api/films", (req, res) => {
+    res.json("ok");
 });
 
-server.get("/api/donnees/:id", (req, res) => {
-    console.log(req.params, req.query);
-    const id = req.params.id; //On récupère le id dynamique de l'url
-    res.json(id);
+server.post("/api/films", (req, res) => {
+    res.json("ok post");
 });
 
-server.get("/api/donnees/:id/:category", (req, res) => {
-    console.log(req.params, req.query);
-    const id = req.params.id; //On récupère le id dynamique de l'url
-    res.json(id);
-});
+// server.get("/api/donnees/:id", (req, res) => {
+//     console.log(req.params, req.query);
+//     const id = req.params.id; //On récupère le id dynamique de l'url
+//     res.json(id);
+// });
+
+// server.get("/api/donnees/:id/:category", (req, res) => {
+//     console.log(req.params, req.query);
+//     const id = req.params.id; //On récupère le id dynamique de l'url
+//     res.json(id);
+// });
 
 //Gestion de l'erreur 404
 server.use((req, res) => {
