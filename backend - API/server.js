a16cbbb1d13acac5dc08eb430e15f6372c1c3c7c
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2024-04-16 16:37:30 -0400

    Cours 3

--- backend - API/server.js
+++ backend - API/server.js
@@ -4,6 +4,7 @@ const path = require("path");
 
 const express = require("express");
 const server = express();
+const db = require("./data/db");
 
 //Doit être défini au début de l'application
 const dotenv = require("dotenv");
@@ -12,35 +13,66 @@ dotenv.config();
 //On indique que le dossier public est accessible à tous
 server.use(express.static(path.join(__dirname, "public")));
 
-// server.get("/", (req, res) => {
-//     let user = {
-//         id: 1,
-//         nom: "Maxime",
-//         email: "mlacassegermain@cmaisonneuve.qc.ca",
-//     };
-//     // res.statusCode = 201; //Change le code de status
-//     res.status(201).json(user); //Renvoie de la données en json
-// });
+server.get("/api/films", async (req, res) => {
+    try {
+        const ordre = req.query.ordre || "asc";
+        const limite = parseInt(req.query.limite) || 10;
 
-server.get("/api/films", (req, res) => {
-    res.json("ok");
+        const references = await db.collection("films").orderBy("titre", ordre).limit(limite).get();
+
+        const filmsTrouves = [];
+
+        references.forEach((doc) => {
+            // const docData = doc.data();
+            // docData.id = doc.id;
+            const docData = { id: doc.id, ...doc.data() };
+            filmsTrouves.push(docData);
+        });
+
+        //Si aucun film trouvé, retourner erreur 404
+        if (filmsTrouves.length == 0) {
+            res.statusCode = 404;
+            return res.json({ message: "Aucun film trouvé" });
+        }
+
+        //Si tout va bien, on envoie les données
+        res.statusCode = 200;
+        return res.json(filmsTrouves);
+    } catch (erreur) {
+        res.statusCode = 500;
+        return res.json({ message: erreur.message });
+    }
+});
+
+server.get("/api/films/:id", async (req, res) => {
+    const id = req.params.id;
+
+    const reference = await db.collection("films").doc(id).get();
+    const data = reference.data();
+
+    res.statusCode = 200;
+    return res.json(data);
 });
 
 server.post("/api/films", (req, res) => {
     res.json("ok post");
 });
 
-// server.get("/api/donnees/:id", (req, res) => {
-//     console.log(req.params, req.query);
-//     const id = req.params.id; //On récupère le id dynamique de l'url
-//     res.json(id);
-// });
+server.post("/api/films/initialiser", (req, res) => {
+    try {
+        const dataInit = require("./data/filmsDepart");
 
-// server.get("/api/donnees/:id/:category", (req, res) => {
-//     console.log(req.params, req.query);
-//     const id = req.params.id; //On récupère le id dynamique de l'url
-//     res.json(id);
-// });
+        dataInit.forEach(async (film) => {
+            await db.collection("films").add(film);
+        });
+
+        res.statusCode = 200;
+        return res.json({ message: "Liste de films initialisée" });
+    } catch (erreur) {
+        res.statusCode = 500;
+        return res.json({ message: erreur.message });
+    }
+});
 
 //Gestion de l'erreur 404
 server.use((req, res) => {
