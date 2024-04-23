28ebe4b5a842a6c56cc477a25867d59fd7e31f7d
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2024-04-23 16:14:25 -0400

    user commentaire

--- backend - API/server.js
+++ backend - API/server.js
@@ -2,6 +2,7 @@ const path = require("path"); //Module de nodejs servant à gérer les chemins r
 const express = require("express"); //Librairie pour utiliser express
 const cors = require("cors"); //Librairie pour permettre les requêtes entre des noms de domaine différents
 const dotenv = require("dotenv"); //Librairie servant à utiliser les variables d'environnements
+const bcrypt = require("bcrypt"); //Librairie pour hasher le mot de passe
 
 //Doit être défini au début de l'application
 const server = express(); //Initialisation du serveur
@@ -56,7 +57,9 @@ server.get("/api/films/:id", async (req, res) => {
     const id = req.params.id;
 
     const reference = await db.collection("films").doc(id).get();
+    // const reference = await db.collection("films").where("courriel","=",req.body.courriel ).get();
     const data = reference.data();
+    data.id = id;
 
     res.statusCode = 200;
     return res.json(data);
@@ -86,6 +89,68 @@ server.post("/api/films/initialiser", (req, res) => {
     } catch (erreur) {
         res.statusCode = 500;
         return res.json({ message: erreur.message });
+    }
+});
+
+server.post("/api/utilisateurs/inscription", async (req, res) => {
+    try {
+        const { courriel, mdp } = req.body;
+        //vérifier si le user existe déjà
+        //Si oui, on retourne un message d'erreur 400
+
+        //Si non,
+
+        //Validation des infos
+
+        //Hasher le mdp
+        const hash = await bcrypt.hash(mdp, 10);
+        const utilisateur = {
+            courriel: courriel,
+            mdp: hash,
+        };
+        //Ajouter à la base de données
+
+        //Retourner un code 201 avec un message de succès
+    } catch (erreur) {
+        //Retourner une erreur 500 avec un message d'erreur
+    }
+});
+
+server.post("/api/utilisateurs/connexion", async (req, res) => {
+    try {
+        //Recupère les données
+        const { courriel, mdp } = req.body;
+        // const courriel = req.body.courriel;
+
+        //Validation
+
+        //Si l'utilisateur existe
+
+        // Si non, on retourne un message d'erreur 400
+
+        //Si oui, on récupère le premier utilisateur
+
+        //On hash le mdp et on compare
+        const user = { mdp: "sjdfhkksjdhfkjdshf" };
+        const resultatComparaison = await bcrypt.compare(mdp, user.mdp);
+
+        //Si le mot de passe ne concorde pas
+        //On renvoie un message d'erreur 400
+        if (resultatComparaison == false) {
+            res.statusCode = 400;
+            return res.json({ message: "Le mots de passe ne concorde pas" });
+        }
+        //Si tiguidou,
+        //On renvoie l'utilisateur (sans le mot de passe) et le token
+        delete user.mdp; //L'opérateur delete permet de supprimer une propriété d'un objet JS
+        const dataARenvoyer = {
+            utilisateur: user,
+            jeton: "",
+        };
+        res.statusCode = 200;
+        return res.json(dataARenvoyer);
+    } catch (erreur) {
+        //On retourne une erreur 500 et un message d'erreur
     }
 });
 
